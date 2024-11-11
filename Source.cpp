ce9c679f1a953c015032893c13959c17e24d80df
dieflieg <agatachris96@gmail.com>
2024-11-12 00:39:27 +0700

    v?
    итоговый вариант

--- Source.cpp
+++ Source.cpp
@@ -5,13 +5,15 @@
 #include <WinSock.h>
 #pragma comment(lib, "Ws2_32.lib")
 
-void print_MACADDR(FILE* out, char* MAC) {
+// выводит MAC-адрес (6 байт) в читаемом формате XX:XX:XX:XX:XX:XX
+void print_MACADDR(FILE* out, char* MAC) { 
     for (int i = 0; i < 5; i++) {
         fprintf(out, "%02X:", (unsigned char)MAC[i]);
     }
     fprintf(out, "%02X\n", (unsigned char)MAC[5]);
 }
 
+// выводит IP-адрес (4 байта) в формате X.X.X.X.
 void print_IPADDR(FILE* out, char* IP) {
     for (int i = 0; i < 3; i++) {
         fprintf(out, "%d.", (unsigned char)IP[i]);
@@ -56,7 +58,7 @@ int main() {
     int frames = 1;
     size_t type_count[5] = { 0 };
 
-    // Цикл обработки каждого фрейма
+    // Цикл обработки каждого кадра
     while (d < DATA + fsize) {
         fprintf(out, "Кадр: %d\n", frames);
         fprintf(out, "MAC-адрес получателя: ");
@@ -64,42 +66,49 @@ int main() {
         fprintf(out, "MAC-адрес отправителя: ");
         print_MACADDR(out, d + 6); // следующие 6 байтов - адрес отправителя
 
-        unsigned short type = ntohs(*(unsigned short*)(d + 12)); // определяем тип или длину фрейма
-        int frame_size = 14; // Инициализируем размер заголовком (14 байт)
+        unsigned short type = ntohs(*(unsigned short*)(d + 12)); // определяем тип/длину кадра
+        int frame_size = 14; // Инициализируем размер заголовком Ethernet (14 байт = длина мак адресов (6 + 6) + длина поля type/lenght)
 
-        if (type > 0x05DC) { // Ethernet DIX (II)
+        if (type > 0x05DC) { // если значение поля type/lenght > 0x05DC, то это Ethernet DIX (II)
             fprintf(out, "Тип кадра: Ethernet DIX (II)\n");
-            if (type == 0x0800) { // Проверка на IPv4
+
+            if (type == 0x0800) { // проверка, принадлежит ли вложенный пакет типу IPv4
                 fprintf(out, "Type: IPv4\n");
-                int ip_total_length = ntohs(*(unsigned short*)(d + 16)); // длина IP пакета
-                frame_size += ip_total_length; // полный размер фрейма
+                int ip_total_length = ntohs(*(unsigned short*)(d + 16)); // длина IP пакета 
+                frame_size += ip_total_length; // полный размер фрейма = длина Ethernet заголовка + длина вложенного IP пакета
 
                 // Вывод IP-адресов
                 fprintf(out, "IP-адрес отправителя: ");
-                print_IPADDR(out, d + 26); // IP отправителя на 27-30 байтах
+                print_IPADDR(out, d + 26); // IP отправителя на 27-30 байтах кадра
                 fprintf(out, "IP-адрес получателя: ");
-                print_IPADDR(out, d + 30); // IP получателя на 31-34 байтах
+                print_IPADDR(out, d + 30); // IP получателя на 31-34 байтах кадра
+           
             }
             else {
-                frame_size += ntohs(*(unsigned short*)(d + 16)); // полный размер фрейма, если это не IPv4
+                // Обработка ошибки для неизвестного типа
+                fprintf(out, "Ошибка: Сетевой протокол неизвестного типа. Невозможно определить границы следующего кадра.\n");
+                fprintf(out, "\n-------------------------------\n");
+                break; // Выход из цикла обработки фреймов
             }
+
             d += frame_size; // смещаем указатель для перехода к следующему фрейму
             type_count[1]++;
         }
-        else {
-            frame_size += type; // для всех остальных типов длина равна значению поля type
 
-            unsigned short F = ntohs(*(unsigned short*)(d + 14)); // следующие 2 байта после поля type (или length)
+        else {
+            frame_size += type ; // для всех остальных типов длина равна значению поля length + длина Ethernet заголовка
+
+            unsigned short F = ntohs(*(unsigned short*)(d + 14)); // следующие 2 байта после поля length
 
             // Определение типа кадра по 15-16 байтам 
-            if (F == 0xFFFF) {
+            if (F == 0xFFFF) { // поле данных кадра Raw 802.3 всегда начинается с двух байтов, содержащих 0хFFFF
                 fprintf(out, "Тип кадра: Raw 802.3/Novell 802.3\n");
                 type_count[2]++;
             }
-            else if (F == 0xAAAA) {
+            else if (F == 0xAAAA) { // поля DSAP и SSAP заголовка LLC оба имеют код 0хАА, отведенный для протокола SNAP
                 fprintf(out, "Тип кадра: Ethernet SNAP\n");
 
-                // Проверка на IPv4 для Ethernet SNAP
+                // проверка, принадлежит ли пакет, вложенный в кадр  Ethernet SNAP, типу IPv4
                 if (ntohs(*(unsigned short*)(d + 20)) == 0x0800) { // 21-22 байты
                     fprintf(out, "Type: IPv4\n");
                     // Вывод IP-адресов
@@ -108,17 +117,17 @@ int main() {
                     fprintf(out, "IP-адрес получателя: ");
                     print_IPADDR(out, d + 38); // IP получателя на 39-42 байтах
                 }
-                
+
                 type_count[3]++;
             }
             else {
-                fprintf(out, "Тип кадра: Ethernet LLC\n");
+                fprintf(out, "Тип кадра: 802.3/LLC\n");
                 type_count[4]++;
             }
             d += frame_size; // переход к следующему фрейму, используя длину
         }
 
-        fprintf(out, "Размер фрейма: %d байт\n", frame_size);
+        fprintf(out, "Размер кадра: %d байт\n", frame_size);
         fprintf(out, "\n-------------------------------\n");
         frames++;
     }
@@ -128,7 +137,7 @@ int main() {
     fprintf(out, "Ethernet DIX (II): %d\n", type_count[1]);
     fprintf(out, "Raw 802.3/Novell 802.3: %d\n", type_count[2]);
     fprintf(out, "Ethernet SNAP: %d\n", type_count[3]);
-    fprintf(out, "Ethernet LLC: %d\n", type_count[4]);
+    fprintf(out, "802.3/LLC: %d\n", type_count[4]);
 
     fclose(out);
     delete[] DATA;
